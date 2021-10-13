727d6a032de4ca019967b26a3c7d72ea5c67b6b1
Kleber Tomazi <50743611+KleberTomazi@users.noreply.github.com>
2021-10-13 20:34:33 -0300

    Python/Flask v1.0

---
+++ test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-  
+from app import app                                                                                                                                                                                      
+import unittest                                                                                                                                                                                          
+
+class Test(unittest.TestCase):                                                                                                                                                                           
+
+    def setUp(self):                                                                                                                                                                                     
+        # cria uma instância do unittest, precisa do nome "setUp"                                                                                                                                        
+        self.app = app.test_client()                                                                                                                                                                     
+
+        # envia uma requisição GET para a URL                                                                                                                                                            
+        self.result = self.app.get('/')                                                                                                                                                                  
+
+    def test_requisicao(self):                                                                                                                                                                           
+        # compara o status da requisição (precisa ser igual a 200)                                                                                                                                       
+        self.assertEqual(self.result.status_code, 200)                                                                                                                                                   
+
+    def test_conteudo(self):                                                                                                                                                                             
+        # verifica o retorno do conteúdo da página                                                                                                                                                       
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello World")
